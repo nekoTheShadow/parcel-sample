16caab4133445dee15ef6fc445ff69580d16129e
hajime nakamura <h.nakamura0903@gmail.com>
2020-07-30 00:40:30 +0900

    整理

---
+++ README.md
@@ -0,0 +1,3 @@
+以下のQiita記事を書くために作成したレポジトリ
+
+https://qiita.com/neko_the_shadow/items/2f3a6d8ef547cdf45592
---
+++ parcel-sample/main.js
@@ -0,0 +1,4 @@
+import Word from './word.js'
+
+document.getElementById("title").innerHTML = Word.title
+document.getElementById("description").innerHTML = Word.description
